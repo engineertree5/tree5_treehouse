e4722e700253f3810b21e0baf72d1c4bd6bdc3d2
engineertree5 <engineertree5@gmail.com>
2020-06-07 17:55:15 -0400

    refactoring after review

--- tree_types/money_tree/yfinance/test_yf.py
+++ tree_types/money_tree/yfinance/test_yf.py
@@ -4,10 +4,7 @@ import random
 
 def random_pick():
     watchlist = ['MSFT', 'V', 'DLR', 'CONE', 'PING', 'AMD', 'O', 'BAM', 'DDOG', 'ADBE', 'NKE', 'CHWY', 'NOK']
-    item_count = len(watchlist) - 1
-    print(item_count)
-    randppick = random.randint(0, item_count)
-    stock_pick = watchlist[randppick]
+    stock_pick = random.choice(watchlist)
     print(f'random stock pick from watchlist ${stock_pick}')
     return stock_pick
 
@@ -22,31 +19,25 @@ def pick_info(ticker):
     return company
 
 
-def company_5D_MA(company):
-    hist = company.history(period="5d")
+def company_moving_average(company, days):
+    hist = company.history(period=f"{days}d")
     count = 0
-    for i in range(0,5):
+    for i in range(0, days):
         count += hist.iloc[i]['Close']
-    fiveDMA = count / 5
-    print(f'Here is the 5 day Moving average for {company.ticker}: {fiveDMA}')
-    
-    hist90d = company.history(period="90d")
-    count = 0
-    for i in range(0,90):
-        count += hist90d.iloc[i]['Close']
-    nintyDMA = count / 90
-    print(f'Here is the 90 day Moving average for {company.ticker}: {nintyDMA}')
-    #Pull 5D moving average... what does that look like from alg to code?
+    fiveDMA = count / days
+    print(f'Here is the {days} day Moving average for {company.ticker}: {fiveDMA}')
     # a 5-day simple moving average is the five-day sum of closing prices divided by five.
 
 def main():
     # is this the best way to call everything? 
     ticker = random_pick()
     company = pick_info(ticker)
-    company_5D_MA(company)
+    company_moving_average(company, 5)
     
 if __name__ == "__main__":
     main()
+
+
 
 # # Download stock data then export as CSV
 # data_df = yf.download("AAPL", start="2020-02-01", end="2020-03-20")
