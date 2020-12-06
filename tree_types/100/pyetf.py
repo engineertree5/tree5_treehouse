99c6fdd2f950714433319dcf495268478960e3cb
engineertree5 <engineertree5@gmail.com>
2020-12-06 17:30:51 -0500

    day 5 i think

--- tree_types/100/pyetf.py
+++ tree_types/100/pyetf.py
@@ -17,17 +17,16 @@ from datetime import date
 # and begin to performn technical and fundamental analysis
 ###### SCRIPT SUMMARY ######
 
-# GLOBAL VARS
-today = date.today()
-d_slash = today.strftime("%Y/%m/%d")
-d_dash = today.strftime("%Y-%m-%d")
-
-class pyetf(object):
+class stock_list(object):
     """
-    pytef class is used to pull & chart company financial data.
+    stock_list class is used to pull & chart company financial data.
     Initinatializing requires the following 2 arguments
-    pytef("COMPANY_NAME", "DAYS")
+    stock_list("COMPANY_NAME", "DAYS")
     """
+    # GLOBAL VARS
+    today = date.today()
+    d_slash = today.strftime("%Y/%m/%d")
+    d_dash = today.strftime("%Y-%m-%d")
     def __init__(self, stock_pick, time_length ):
         #        #^ The first variable is the class instance in methods.  
         #        #  This is called "self" by convention, but could be any name you want.
@@ -38,64 +37,57 @@ class pyetf(object):
         self.stock_pick = stock_pick #creates an attribute called stock and is assigned to value of stock
         self.time_length = time_length #creates an attribute called time_length and is assigned to value of time_length
 
-    def set_time(self):
-        #The SELF parameter is a reference to the current instance of the class
-        #and is used to access variables that belong to the class.
-        # we will be using the stock the stock attribute to get more data
-        pass
-
-    def get_stocks(self):
-        """
-        Method for pulling in standard company data. 
-        Method can process N number of stocks for analysis
-        """
-        stock_pick = self.stock_pick
-        for stock in stock_pick:
-            company = yf.Ticker(stock)
-            data = company.history(period=f"{self.time_length}d")
-            try:
-                PS_TTM = company.info["priceToSalesTrailing12Months"]
-                FF2_wkL = company.info["fiftyTwoWeekLow"]
-                FF2_wkH = company.info["fiftyTwoWeekHigh"]
-                FFd_ma = company.info["fiftyDayAverage"]
-                THd_ma = company.info["twoHundredDayAverage"]
-                s_name = company.get_info()['shortName']
-                sector = company.info["sector"]
-                industry = company.info["industry"]
-            except IndexError as err:
-                s_name = '*'
-                print(err)
-        return PS_TTM, FF2_wkH, FF2_wkL, FFd_ma, THd_ma, s_name, sector, industry
-
     def get_market_status(self):
         pass
-    def get_fa(self):        
-        PS_TTM, FF2_wkH, FF2_wkL, FFd_ma, THd_ma, s_name, sector, industry = self.get_stocks()
-        print(f"this is sameple {sector}")
-        return None
-    def get_ta(self):
+        #The SELF parameter is a reference to the current instance of the class
+        #and is used to access variables that belong to the class.
+        # we will be using the stock the stock attribute to get more data        
+    def get_stocks(self):
+        stock_pick = self.stock_pick # value needs to be passed into function
+        for stock in stock_pick:
+            company = yf.Ticker(stock)
+            data = company.history(period=f"{self.time_length}d") # we will be using the data var for TA
+            self.get_fa(company)
+            # self.get_ta(company)
+        pass
+    def get_fa(self, company):
+        """
+        Method for processing company data and extracting 
+        fundamental analysis on it. Assuming company = yf.Ticker(company) 
+        """
+        self.company = company
+        try:
+            symbol = company.info["symbol"]
+            sector = company.info["sector"]
+            industry = company.info["industry"]
+            PS_TTM = company.info["priceToSalesTrailing12Months"]
+            s_name = company.get_info()['shortName']
+            FF2_wkL = company.info["fiftyTwoWeekLow"]
+            FF2_wkH = company.info["fiftyTwoWeekHigh"]
+            FFd_ma = company.info["fiftyDayAverage"]
+            THd_ma = company.info["twoHundredDayAverage"]
+            print(f'\nPulling data for {s_name}\n***********')
+            print(f'{s_name.upper()}')
+            print(f'Sector: {sector}')
+            print('PS/TTM:', ('${:,.2f}'.format(PS_TTM)))
+            print('52 week low:', ('${:,.2f}'.format(FF2_wkL)))
+            print('52 wk high:', ('${:,.2f}'.format(FF2_wkH)))
+            print('50d MA:', ('${:,.2f}'.format(FFd_ma)))
+            print('200d MA:', ('${:,.2f}'.format(THd_ma)))
+        except IndexError as err:
+            s_name = '*'
+            print(err)
+    
+        
+    def get_ta(self, data):
+        self.data = data
         pass
     def create_ta_charts(self):
         pass
-    # def get_basic_info(self, get_stocks):
-    #     main = self.main
-    #     print(f'\nPulling data for {s_name}\n***********\n')
-    #     print(f'{s_name}')
-    #     print(f'{sector}')
-    #     print('PS/TTM:', ('${:,.2f}'.format(PS_TTM)))
-    #     print('52 week low:', ('${:,.2f}'.format(FF2_wkL)))
-    #     print('52 wk high:', ('${:,.2f}'.format(FF2_wkH)))
-    #     print('50d MA:', ('${:,.2f}'.format(FFd_ma)))
-    #     print('200d MA:', ('${:,.2f}'.format(THd_ma)))
-    #     return None
 
 def main():
-
 #### TESTING SECTION ####
-    my_stocks = ['SHOP'] #Simple watchlist 
-
-    sample = pyetf(my_stocks, 365) #sample instance of pyetf class
-    print(sample.get_fa())
-    
-    
+    my_stocks = ['SHOP', 'NET', 'AMD'] #Simple watchlist 
+    f_analysis = stock_list(my_stocks, 365) #we instantiate the stock_list class, passing in mystocks and a set time frame
+    f_analysis.get_stocks()
 main()
